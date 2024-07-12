c63546f2708607c15b1be8ee8e097fdd6b84a759
Dmitriy-Grabovskiy <109272784+Dmitriy-Grabovskiy@users.noreply.github.com>
2024-07-12 18:11:09 +0300

    fix 8

--- src/formatter/index.js
+++ src/formatter/index.js
@@ -1,14 +1,14 @@
 import stylish from './stylish.js';
-//import plain from './plain.js';
+import plain from './plain.js';
 import json from './json.js';
 
 export default (file, format = 'stylish') => {
   if (format === 'stylish') {
     return stylish(file);
   }
- /* if (format === 'plain') {
+  if (format === 'plain') {
     return plain(file);
-  }*/
+  }
   if (format === 'json') {
     return json(file);
   }
