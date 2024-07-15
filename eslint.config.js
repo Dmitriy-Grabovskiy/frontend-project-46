563409a80268dff9e75af10561e8103978960328
Dmitriy-Grabovskiy <109272784+Dmitriy-Grabovskiy@users.noreply.github.com>
2024-07-15 12:40:49 +0300

    fix13

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,45 @@
-import globals from "globals";
-
-import path from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import pluginJs from "@eslint/js";
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 // mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+const compat = new FlatCompat({
+  baseDirectory: dirname, // optional; default: process.cwd()
+  resolvePluginsRelativeTo: dirname, // optional
+  recommendedConfig: js.configs.recommended, // optional unless you're using "eslint:recommended"
+  allConfig: js.configs.all // optional unless you're using "eslint:all"
+});
 
 export default [
-    {languageOptions: { globals: globals.browser }},
-    ...compat.extends("airbnb-base"),
-    {
-        rules: {
-            "no-console": "off",
-            "import/extensions": "off",
-            "linebreak-style": "off",
-            "import/no-named-as-default": "off",
-            "import/no-named-as-default-member": "off",
-            "eqeqeq": "off",
-        }
+
+  // mimic ESLintRC-style extends
+  ...compat.extends('airbnb-base'),
+
+  // mimic environments
+  ...compat.env({
+    es2020: true,
+    node: true
+  }),
+
+  // translate an entire config
+  ...compat.config({
+    extends: 'airbnb-base/legacy',
+    env: {
+      es2020: true,
+      node: true
     },
+    rules: {
+      'airbnb-base/arrow-body-style': 'off',
+      'no-console': 'off',
+      'import/extensions': 'off',
+      'linebreak-style': 'off',
+      'import/no-named-as-default': 'off',
+      'import/no-named-as-default-member': 'off',
+      'import/no-extraneous-dependencies': 'off'
+    }
+  })
 ];
